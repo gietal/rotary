623a536404dd51b34ef3c7c727c68b7c7ff540d5
gietal <gieta.laksmana@hotmail.com>
2016-08-24 00:23:42 -0700

    player spawner

---
+++ rotary/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Rotary 
+{
+    
+    public class PlayerSpawner : MonoBehaviour {
+
+        public GameObject playerPrefab = null;
+        public GameObject cameraPrefab = null;
+
+        void Awake()
+        {
+            Debug.Assert(playerPrefab != null);
+            Debug.Assert(cameraPrefab != null);
+
+            var player = (GameObject)GameObject.Instantiate(playerPrefab, this.transform.position, this.transform.rotation);
+            var camera = (GameObject)GameObject.Instantiate(cameraPrefab, this.transform.position, this.transform.rotation);
+
+            var follow = camera.GetComponent<UnityStandardAssets.Cameras.AutoCam>();
+            follow.SetTarget(player.transform);
+        }
+    }
+}
