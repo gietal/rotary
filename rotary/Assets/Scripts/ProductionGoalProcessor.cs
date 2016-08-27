224352bdad87f0ec0dd5fefa6456e869fa591c6d
gietal <gieta.laksmana@hotmail.com>
2016-08-27 15:53:37 -0700

    production managers loads next level

---
+++ rotary/Assets/Scripts/ProductionGoalProcessor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+
+public class ProductionGoalProcessor : IGoalDelegate 
+{
+    public override void OnPlayerEnterGoal(GameObject thePlayer)
+    {
+        var activeScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeScene + 1);
+    }
+}
