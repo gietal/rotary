1ebd88c4b22b5f500fcb06ae185693eaef5b124b
gietal <gieta.laksmana@hotmail.com>
2016-08-27 16:44:14 -0700

    level 4

---
+++ rotary/Assets/Scripts/ProductionInitializer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProductionInitializer : MonoBehaviour 
+{
+    public GameObject worldPrefab = null;
+
+    void Awake()
+    {
+        Debug.AssertFormat(worldPrefab != null, "must have world prefab");
+    }
+    // after everything is initialized
+    // spawn a world object
+    void Start()
+    {
+        GameObject.Instantiate(worldPrefab, Vector3.zero, Quaternion.identity);
+    }
+}
