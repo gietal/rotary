45c5897069656ee764b0a62b1f228e3f38259410
gietal <gieta.laksmana@hotmail.com>
2016-08-28 11:58:15 -0700

    ui control complete

--- rotary/Assets/Scripts/ProductionInitializer.cs
+++ rotary/Assets/Scripts/ProductionInitializer.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class ProductionInitializer : MonoBehaviour 
 {
     public GameObject worldPrefab = null;
+    public GameObject uiControlPrefab = null;
 
     void Awake()
     {
         Debug.AssertFormat(worldPrefab != null, "must have world prefab");
+
+        #if UNITY_ANDROID || UNITY_IOS
+        Debug.AssertFormat(uiControlPrefab != null, "mobile app must have UI control prefab");
+        #endif
     }
     // after everything is initialized
     // spawn a world object
     void Start()
     {
         GameObject.Instantiate(worldPrefab, Vector3.zero, Quaternion.identity);
+
+        #if UNITY_ANDROID || UNITY_IOS
+        GameObject.Instantiate(uiControlPrefab, Vector3.zero, Quaternion.identity);
+        #endif
     }
 }
